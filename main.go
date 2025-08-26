a8c827931f2d8a67ee4f51a41f52b68816cb4cd4
joseph0x45 <zozozozeph@gmail.com>
2025-08-26 09:13:13 +0000

    nuke old repo

--- main.go
+++ main.go
@@ -1,62 +1,15 @@
 package main
 
 import (
-	"embed"
-	"io/fs"
 	"log"
 	"net/http"
-	"os"
-	"server/handler"
-	"server/store"
 	"time"
 
-	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-//go:embed static/*
-var staticFiles embed.FS
-
-//go:embed templates/*
-var templatesFS embed.FS
-
-func getDBConnection() *sqlx.DB {
-	dbURL := os.Getenv("DB_URL")
-	if dbURL == "" {
-		dbURL = "db.sqlite"
-	}
-	db, err := sqlx.Connect("sqlite3", dbURL)
-	if err != nil {
-		panic(err)
-	}
-	log.Println("[INFO] Connected to Database:", dbURL)
-	return db
-}
-
 func main() {
-	db := getDBConnection()
-	printerPort := os.Getenv("PRINTER")
-  log.Printf("Using %s as printer port\n", printerPort)
-	staticContent, err := fs.Sub(staticFiles, "static")
-	if err != nil {
-		log.Println("[ERROR] Error while loading static files")
-		panic(err)
-	}
-
-	store := store.NewStore(db)
-	handler := handler.NewHandler(store, &templatesFS, printerPort)
-
 	mux := http.NewServeMux()
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticContent))))
-	mux.HandleFunc("GET /home", handler.RenderHomePage)
-	mux.HandleFunc("GET /products", handler.RenderProductsPage)
-	mux.HandleFunc("GET /orders", handler.RenderOrdersPage)
-	mux.HandleFunc("GET /stats", handler.RenderStatsPage)
-
-	mux.HandleFunc("POST /api/products", handler.CreateProduct)
-	mux.HandleFunc("POST /api/orders", handler.SaveOrder)
-	mux.HandleFunc("DELETE /api/orders/{id}", handler.DeleteOrder)
-	mux.HandleFunc("GET /api/orders/{id}/print", handler.PrintOrder)
 
 	server := http.Server{
 		Addr:         ":8080",
